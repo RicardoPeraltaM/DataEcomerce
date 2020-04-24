69507065811796a5af122955cb6dc0c74cb59f51
RicardoPeraltaM <r.peralta.rp70@gmail.com>
2020-04-23 23:31:20 -0500

    Data para app CODELLEGE

---
+++ Json/data.ts
@@ -0,0 +1,205 @@
+
+const Laptops = [
+    {
+        id: 1,
+        brand: 1,
+        description:'Pavilion, 1TB+128GB SSD, Blue(15-cw1004la)', 
+        display:'15"',
+        processor:'AMD Ryzen 5 3500U(2a)',
+        ram: '12GB',
+        rating:4.5,
+        img:'../img/1imgHP.jpg',
+        price: 14320.98,
+        discount: 3
+
+    },
+    {
+        id: 2,
+        brand: 5,
+        description:'IdeaPad S145-14AST 500GB Win10 Home 81ST0000LM', 
+        display:'14"',
+        processor:'AMD 2.30 GHz',
+        ram: '4GB',
+        rating:4,
+        img:'../img/2imgLenovo.jpg',
+        price: 6699.00,
+        discount: 2
+
+    },
+    {
+        id: 3,
+        brand: 3,
+        description:'Acer Aspire E 15', 
+        display:'15.6"',
+        processor:'Intel Core i5 8º generación',
+        ram: '8GB',
+        rating:4.5,
+        img:'../img/3imgAcer.jpg',
+        price: 14599.00,
+        discount: 4
+
+    },
+    {
+        id: 4,
+        brand: 2,
+        description:'Huawei MateBook D , Windows 256 GB, Color Plateado', 
+        display:'15.6"',
+        processor:'AMD Ryzen',
+        ram: '8GB',
+        rating:4.5,
+        img:'../img/4imgHuawei.jpg',
+        price: 11999.00,
+        discount: 4
+
+    },
+    {
+        id: 5,
+        brand: 4,
+        description:'LAPTOP GAMING G3", GEFORCE GTX 1050 , 256 SSD', 
+        display:'15.6"',
+        processor:'CORE i5 9 Gen',
+        ram: '8GB',
+        rating:4.5,
+        img:'../img/5imgDELL.jpg',
+        price: 17199.20,
+        discount: 1
+
+    },
+    {
+        id: 6,
+        brand: 3,
+        description:'Acer Spin 3 Convertible Laptop, 512GB PCIe NVMe SSD, Backlit KB', 
+        display:'14"',
+        processor:'Intel CORE i7 8th Gen',
+        ram: '16GB',
+        rating:5,
+        img:'../img/6imgAcer.jpg',
+        price: 21289.69,
+        discount: 1
+
+    },
+    {
+        id: 7,
+        brand: 5,
+        description:'81FB0008LM " Bluetooth+ Wi-Fi, Windows 10, Midnight Blue', 
+        display:'15.6"',
+        processor:'AMD none 2.4GHz',
+        ram: '8GB',
+        rating:4,
+        img:'../img/7imgLenovo.jpg',
+        price: 10899.00,
+        discount: 4
+
+    },
+    {
+        id: 8,
+        brand: 1,
+        description:'HP 15-cx0001la Gaming, 1TB HDD, NVIDIA GeForce GTX 1050, Windows 10', 
+        display:'15.6"',
+        processor:'Intel Core i5 2.3GHz',
+        ram: '8GB',
+        rating:5,
+        img:'../img/8imgHP.jpg',
+        price: 19435.96,
+        discount: 1
+
+    },
+]
+
+const headphones = [
+    {
+        id:1,
+        brand:6,
+        description:'Solo Pro',
+        price:6499,
+        img:'./../img/1imgbeats.png',
+        rating: 4
+    },
+    {
+        id:2,
+        brand:6,
+        description:'Studio3 Wireless',
+        price:6999,
+        img:'./../img/2imgbeats.png',
+        rating: 5
+    },
+    {
+        id:3,
+        brand:6,
+        description:'Solo3 Wireless',
+        price:4499,
+        img:'./../img/3imgbeats.png',
+        rating: 5
+    },
+    {
+        id:4,
+        brand:6,
+        description:'EP',
+        price:1999,
+        img:'./../img/4imgbeats.png',
+        rating: 4
+    },
+    {
+        id:5,
+        brand:6,
+        description:'Pro',
+        price:7799,
+        img:'./../img/5imgbeats.png',
+        rating: 5
+    },
+    {
+        id:6,
+        brand:6,
+        description:'Powerbeats',
+        price:2899,
+        img:'./../img/6imgbeats.png',
+        rating: 4
+    }
+]
+
+const Brands = [
+    {
+        id: 1,
+        brand: 'HP'
+    },
+    {
+        id: 2,
+        brand: 'Huawei'
+    },
+    {
+        id: 3,
+        brand: 'Acer'
+    },
+    {
+        id: 4,
+        brand: 'DELL'
+    },
+    {
+        id: 5,
+        brand: 'Lenovo'
+    }
+    {
+        id: 6,
+        brand: 'beats'
+    }
+
+]
+
+const discounts = [
+    {
+        id: 1,
+        percentage: 5
+    },
+    {
+        id: 2,
+        percentage: 10
+    },
+    {
+        id: 3,
+        percentage: 15
+    },
+    {
+        id: 4,
+        percentage: 20
+    }
+]
